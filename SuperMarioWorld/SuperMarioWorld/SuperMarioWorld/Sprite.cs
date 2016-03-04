d39daab80979191d06f8045e0bcf81b466943407
D Inbound <D_Inbound@hotmail.com>
2016-03-04 14:58:38 +0100

    Added the "Sprite" class

---
+++ SuperMarioWorld/SuperMarioWorld/SuperMarioWorld/Sprite.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace SuperMarioWorld
+{
+    class Sprite
+    {
+        public float animationSpeed;
+        private SpriteBatch _spriteBatch;
+        private Texture2D _texture;
+        public string sourceName;
+        
+        public Sprite(Texture2D texture, SpriteBatch spriteBatch)
+        {
+            _spriteBatch = spriteBatch;
+            _texture = texture;
+        }
+
+        public void Draw(Vector2 pos)
+        {
+
+        }
+
+        public void SetAnimation()
+        {
+
+        }
+    }
+}
