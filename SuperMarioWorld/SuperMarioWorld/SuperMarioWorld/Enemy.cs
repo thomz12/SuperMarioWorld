73336b2ba468f355f0a06848e619230bfa4da36c
D Inbound <D_Inbound@hotmail.com>
2016-03-07 15:09:27 +0100

    Wohoo we got 1 enemy!

---
+++ SuperMarioWorld/SuperMarioWorld/SuperMarioWorld/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperMarioWorld
+{
+    class Enemy : Entity
+    {
+        protected bool flying;
+        protected bool huge;
+
+        protected GameObject drop;
+
+        public Enemy(Vector2 position) : base (position)
+        {
+
+        }
+
+        public void Death()
+        {
+
+        }
+
+        protected override void Movement(GameTime gameTime)
+        {
+            base.Movement(gameTime);
+        }
+    }
+}
