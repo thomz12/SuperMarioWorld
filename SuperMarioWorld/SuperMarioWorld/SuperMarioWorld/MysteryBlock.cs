6a773ae9a549783716e29a58fe45dedd68a43c23
D Inbound <D_Inbound@hotmail.com>
2016-03-19 16:46:13 +0100

    Edited the behaviour of the mystery block when it is used.

--- SuperMarioWorld/SuperMarioWorld/SuperMarioWorld/MysteryBlock.cs
+++ SuperMarioWorld/SuperMarioWorld/SuperMarioWorld/MysteryBlock.cs
@@ -63,13 +63,16 @@ namespace SuperMarioWorld
                         p.position.Y = position.Y + p.boundingBox.Height;
                         if (p is Player)
                         {
-                            sprite.NewAnimation();
-                            sprite.AddFrame(4, 0);
+                            //When player collides from the bottom
+                            create(new StaticBlock(position, StaticBlock.BlockType.used, 0.5f));
+
                             if (_content != null)
                             {
                                 create(_content);
                                 _content = null;
                             }
+
+                            destory(this);
                         }
 
                         p.momentum.Y = 16;
