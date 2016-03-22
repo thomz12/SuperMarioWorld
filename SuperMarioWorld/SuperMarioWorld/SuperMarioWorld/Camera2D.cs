45223a138bf5b8ad982b1335a0dcc64b31a1b1ad
D Inbound <D_Inbound@hotmail.com>
2016-03-22 12:43:56 +0100

    bunch o stuff

--- SuperMarioWorld/SuperMarioWorld/SuperMarioWorld/Camera2D.cs
+++ SuperMarioWorld/SuperMarioWorld/SuperMarioWorld/Camera2D.cs
@@ -38,7 +38,6 @@ namespace SuperMarioWorld
         private float _smoothness = 4f;
 
         private float _xDeadZone = 32.0f;
-        private bool movingRight;
 
         private Point _levelSize;
         private int _gridSize;
@@ -69,55 +68,55 @@ namespace SuperMarioWorld
         /// </summary>
         public void Update(GameTime gameTime)
         {
+            //check if the target exists
             if (_target != null)
             {
+                //Check if the camera is allowed to move
                 if (_moveable)
                 {
+                    //Difference between camera and target position
                     Vector2 delta = _target.position - Position;
 
+                    //The camera uses smooth damping.
+                    float smoothDamping = _smoothness * ((float)gameTime.ElapsedGameTime.TotalMilliseconds / 1000);
+
                     //X axis
-                    if (delta.X < -_xDeadZone)
-                        movingRight = false;
-                    if (delta.X > _xDeadZone)
-                        movingRight = true;
-
-                    float targetX = _target.position.X;
-
-                    if (movingRight)
+                    //If the player moves out of the dead zone, move the camera to the player
+                    if (delta.X < -_xDeadZone || delta.X > _xDeadZone)
                     {
-                        if (delta.X > 0)
-                            Position = new Vector2(Position.X + (targetX - Position.X) * _smoothness * ((float)gameTime.ElapsedGameTime.TotalMilliseconds / 1000f), Position.Y);
+                        Position = new Vector2(Position.X + (_target.position.X - Position.X) * smoothDamping, Position.Y);
+                    }
 
-                    }
-                    else
-                    {
-                        if (delta.X < 0)
-                            Position = new Vector2(Position.X + (targetX - Position.X) * _smoothness * ((float)gameTime.ElapsedGameTime.TotalMilliseconds / 1000f), Position.Y);
-                    }
+                    //Limit the camera to the left edge of the map
+                    if (Position.X < GameWidth / 2 - _gridSize / 2)
+                        Position = new Vector2(GameWidth / 2 - _gridSize / 2, Position.Y);
+                    //Limit the camera to the right edge of the map
+                    if (Position.X > _levelSize.X * _gridSize - GameWidth / 2 - _gridSize / 2)
+                        Position = new Vector2(_levelSize.X * _gridSize - GameWidth / 2 - _gridSize / 2, Position.Y);
 
                     //Y axis
+                    //if the lower bounds of the camera vieuwport is lower than the lowest pixel in the level (y+ == lower)
                     if (Position.Y + GameHeight / 2 >= _levelSize.Y * _gridSize)
                     {
+                        //set the camera position to snap to the bottom edge (use levelSize for Y coord instead of player Y)
                         Position = new Vector2(Position.X, _levelSize.Y * _gridSize - GameHeight / 2);
+
+                        //If the player is in the top 1/4th half of the screen, the camera breaks free from the snap.
                         if (delta.Y < -(GameHeight / 4))
                         {
-                            Position = new Vector2(Position.X, Position.Y + (_target.position.Y - Position.Y) * _smoothness * ((float)gameTime.ElapsedGameTime.TotalMilliseconds / 1000f));
+                            Position = new Vector2(Position.X, Position.Y + (_target.position.Y - Position.Y) * smoothDamping);
                         }
                     }
                     else
                     {
-                        Position = new Vector2(Position.X, Position.Y + (_target.position.Y - Position.Y) * _smoothness * ((float)gameTime.ElapsedGameTime.TotalMilliseconds / 1000f));
+                        //If the camera not beneath the lower bounds of the level, the position of the camera is the position of the player.
+                        Position = new Vector2(Position.X, Position.Y + (_target.position.Y - Position.Y) * smoothDamping);
                     }
-
-                    if (Position.X < GameWidth / 2 - _gridSize / 2)
-                        Position = new Vector2(GameWidth / 2 - _gridSize / 2, Position.Y);
-
-                    if (Position.X > _levelSize.X * _gridSize - GameWidth / 2 - _gridSize / 2)
-                        Position = new Vector2(_levelSize.X * _gridSize - GameWidth / 2 - _gridSize / 2, Position.Y);
                 }
             }
             else
             {
+                //Set the position of the camera to the center of the level
                 Position = new Vector2(_levelSize.X * _gridSize / 2 - _gridSize / 2, _levelSize.Y * _gridSize / 2);
             }
         }
---
+++ SuperMarioWorld/UnitTestProject1/UTCamera2D.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xna.Framework;
+
+namespace SuperMarioWorld
+{
+    [TestClass]
+    public class UTCamera2D
+    {
+        //Camera Variables
+        public Vector2 cameraPosition;
+        public float xDeadZone;
+        public Vector2 delta;
+        public bool movingRight;
+
+        //Target Variables
+        public Vector2 targetPosition;
+        float targetX;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            //Camera
+            cameraPosition = new Vector2(1, 0);
+            xDeadZone = 5f;
+
+            //Target
+            targetPosition = new Vector2(7, 0);
+            targetX = targetPosition.X;
+
+            //Calculate here
+            delta = targetPosition - cameraPosition;
+        }
+
+        [TestMethod]
+        public void Camera2DMovingLeft()
+        {
+            //Check is camera is outside of the left deadzone bounds
+            Assert.IsFalse(delta.X < -xDeadZone);
+        }
+
+        [TestMethod]
+        public void Camera2DMovingRight()
+        {
+            //Check if camera is outside of the right deadzone bounds
+            Assert.IsTrue(delta.X > xDeadZone);
+        }
+
+        [TestMethod]
+        public void Camera2DAdjustPositionIfoutOFLeftBounds()
+        {
+            targetPosition = new Vector2(-10, 0);
+            cameraPosition = new Vector2(0, 0);
+            //delta.x is -10
+            delta = targetPosition - cameraPosition;
+
+            //Deadzone is 5
+            //delta.X < - xDeadZone returns true
+            if (delta.X < -xDeadZone)
+            {
+                movingRight = false;
+            }
+
+            //moving right is false
+            if (movingRight)
+            {
+                //this is not called
+            }
+            else
+            {
+                //This IS called
+                //delta.X < 0 returns true
+                Assert.IsTrue(delta.X < 0);
+            }
+        }
+
+        [TestMethod]
+        public void Camera2DAdjustPositionIfOutOfRightBounds()
+        {
+            targetPosition = new Vector2(1, 0);
+            cameraPosition = new Vector2(7, 0);
+
+            //delta.X = 6
+            delta = targetPosition - cameraPosition;
+
+            //xDeadZone is 5
+            //delta.X > xDeadZone returns true
+            if (delta.X > xDeadZone)
+            {
+                movingRight = true;
+            }
+
+            //movingRight = true;
+            if (movingRight)
+            {
+                //This IS called
+                Assert.IsTrue(delta.X > 0);
+            }
+        }
+
+        [TestMethod]
+        public void Camera2DAdjustPositionYAxisSnapToBottomOfLevel()
+        {
+            Point levelSize = new Point(10, 10);
+            float gridSize = 32;
+
+            targetPosition = new Vector2(5 * gridSize, 2 * gridSize);
+            cameraPosition = new Vector2(5 * gridSize, 2 * gridSize);
+
+            float cameraHeight = 3f * gridSize;
+
+            //If 112 >= 320
+            if (cameraPosition.Y + cameraHeight / 2 >= levelSize.Y * gridSize)
+            {
+
+            }
+        }
+    }
+}
