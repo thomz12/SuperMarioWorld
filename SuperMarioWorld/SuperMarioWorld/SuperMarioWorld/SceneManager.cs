fb0ebba4503fba2089f2cd73eb7c7f2dd4d1fd73
thomz12 <thomz12@hotmail.nl>
2016-03-30 10:39:10 +0200

    Added scene manager singleton

---
+++ SuperMarioWorld/SuperMarioWorld/SuperMarioWorld/SceneManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperMarioWorld
+{
+    public class SceneManager
+    {
+        private static SceneManager instance;
+
+        public static SceneManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new SceneManager();
+                return instance;
+            }
+        }
+        
+        public SceneManager()
+        {
+
+        }
+
+        public void LoadScene()
+        {
+
+        }
+    }
+}
