0785d7a2ad13b1ea741aa3075faf8c1232a2a393
thomz12 <thomz12@hotmail.nl>
2016-03-30 11:29:45 +0200

    Fixed UTScene

--- SuperMarioWorld/Unit Test/UTScene.cs
+++ SuperMarioWorld/Unit Test/UTScene.cs
@@ -14,23 +14,14 @@ namespace SMWUT
         [TestInitialize]
         public void Initialize()
         {
-            Scene.LoadScene loadScene = new Scene.LoadScene(DoNothing);
-
             score = new ScoreHandler();
-            scene = new Scene("Level01", score, loadScene, false);
-        }
-
-        public void DoNothing(string s, bool b)
-        {
-            return;
+            scene = new Scene("Level01", score, false);
         }
 
         [TestMethod]
         public void UTSceneConstructor()
         {
-            Scene.LoadScene loadScene = new Scene.LoadScene(DoNothing);
-
-            Scene s = new Scene("Level01", score, loadScene, false);
+            Scene s = new Scene("Level01", score, false);
             Assert.IsNotNull(s);
         }
     }
