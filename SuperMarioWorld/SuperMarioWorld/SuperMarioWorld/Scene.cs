879d8a1f90ca5766e027df6710e17e96b935adbf
D Inbound <D_Inbound@hotmail.com>
2016-03-31 12:24:47 +0200

    added comments in scene

--- SuperMarioWorld/SuperMarioWorld/SuperMarioWorld/Scene.cs
+++ SuperMarioWorld/SuperMarioWorld/SuperMarioWorld/Scene.cs
@@ -11,30 +11,29 @@ using Microsoft.Xna.Framework.Content;
 
 namespace SuperMarioWorld
 {
-
     public class Scene
     {
-        //Content manager
+        // Content manager
         private ContentManager _contentManager;
 
-        //HUD
+        // HUD
         private HUD _hud;
 
-        //Score handler
+        // Score handler
         private ScoreHandler _scores;
 
-        //Size of the level.
+        // Size of the level.
         private Point _size;
 
-        //Size of the grid
+        // Size of the grid
         private int _gridSize;
 
-        //Camera object
+        // Camera object
         public Camera2D cam;
-        //player object
+        // Player object
         private GameObject _player;
 
-        //Is the level in edit mode?
+        // Is the level in edit mode?
         private bool _edit;
         private string _name;
 
@@ -43,16 +42,17 @@ namespace SuperMarioWorld
         /// </summary>
         public List<GameObject> objects = new List<GameObject>();
 
-        //Dictionary of all the sprites that the gameobjects use, so they dont have to be loaded in multiple times
+        // Dictionary of all the sprites that the gameobjects use, so they dont have to be loaded in multiple times
         private Dictionary<string, Texture2D> loadedSprites = new Dictionary<string, Texture2D>();
 
-        //Variables for the background
+        // Variables for the background
         private string _backgroundSourceName;
         private Texture2D _backgroundTexture;
 
-        //Position of the background texture
+        // Position of the background texture
         private int _backOffset;
 
+        // This list contains all the gameobjects in the current scene
         private List<GameObject> _collidables = new List<GameObject>();
 
         /// <summary>
@@ -61,27 +61,28 @@ namespace SuperMarioWorld
         /// <param name="fileName">Give the name of the file without extension</param>
         public Scene(string fileName, ScoreHandler scoreHandler, bool edit)
         { 
-            //Set score handler
+            // Set score handler
             _scores = scoreHandler;
             _scores.maxTime = 200;
 
+            // Set if the scene is in edit mode
             _edit = edit;
 
-            //Set background texture
+            // Set background texture
             _backgroundSourceName = @"Background\BushBackground";
 
-            //Set the gridsize
+            // Set the gridsize
             _gridSize = 16;
             _backOffset = 0;
 
-            //Add file path to lvl name
+            // Add file path to lvl name
             fileName = @"Content\Levels\" + fileName;
             if (!fileName.Contains(".sml"))
                 fileName += ".sml";
 
             _name = fileName;
 
-            //
+            //Create all objects and add them to the objects array
             try
             {
                 //Create new xml document (to read XML)
@@ -105,7 +106,7 @@ namespace SuperMarioWorld
                     pos.X = int.Parse(node.LastChild.Attributes["xPos"].Value);
                     pos.Y = int.Parse(node.LastChild.Attributes["yPos"].Value);
 
-                    //Check element name, and create object
+                    //Check element name, and create object corresponding to that name
                     if (node.Name.Equals("MainMenu"))
                     {
                         obj = new MainMenu(pos, _contentManager);
@@ -113,14 +114,18 @@ namespace SuperMarioWorld
                     }
                     else if (node.Name.Equals("Player"))
                     {
+                        // Check if the game is not in edit mode
                         if (!edit)
                         {
+                            // Generate a random number for a random player
                             Random r = new Random();
+                            // Create a random player
                             obj = new Player(pos, _scores, (Player.Character)r.Next(0, Enum.GetNames(typeof(Player.Character)).Length));
                             _player = (Player)obj;
                         }
                         else
                         {
+                            // Create a builder if the game is in edit mode
                             obj = new Builder(pos, _size, _gridSize);
                             _player = obj;
                         }
@@ -139,8 +144,10 @@ namespace SuperMarioWorld
                     }
                     else if (node.Name.Equals("MysteryBlock"))
                     {
+                        // Create new gameobject
                         GameObject content;
 
+                        // Get the node for content
                         if (node.FirstChild.Attributes[0].Value.Equals("Mushroom"))
                             content = new Mushroom(Point.Zero);
                         else if (node.FirstChild.Attributes[0].Value.Equals("OneUp"))
@@ -148,6 +155,7 @@ namespace SuperMarioWorld
                         else
                             content = new Coin(Point.Zero, true);
 
+                        // Create the mystery block with correct content
                         obj = new MysteryBlock(pos, content);
                     }
                     else if (node.Name.Equals("StaticBlock"))
@@ -177,36 +185,33 @@ namespace SuperMarioWorld
 
                     if (obj != null)
                     {
+                        // Add the delecates to the gameobjects
                         obj.create = new GameObject.CreateObject(CreateObject);
                         obj.destroy = new GameObject.DestroyObject(DestroyObject);
+                        // Add the object to the objects array
                         objects.Add(obj);
                     }
                 }
 
-                //Create HUD object
+                // Create HUD object when a player is present
                 if (_player is Player)
                 {
-                    Player p = (Player)_player;
-                    _hud = new HUD(_scores, p);
+                    Player player = (Player)_player;
+                    _hud = new HUD(_scores, player);
                 }
                 else
                 {
+                    // Create an hud without a player
                     _hud = new HUD(_scores, null);
                 }
 
-                //Create camera object
-                if (_player != null)
-                {
-                    cam = new Camera2D(_player, _size, _gridSize);
-                }
-                else
-                {
-                    cam = new Camera2D(null, _size, _gridSize);
-                }
+                // Create camera object
+                cam = new Camera2D(_player, _size, _gridSize);
             }
             //something went wrong, we have to do something here
             catch(Exception e)
             {
+                // Go to Thom for help (cuz he needs to fix his shit then)
                 throw e;
             }
         }
@@ -217,12 +222,13 @@ namespace SuperMarioWorld
         /// <param name="contentManager"></param>
         public void LoadContent(ContentManager contentManager)
         {
-            //Set a content manager so new items can be spawned in.
+            // Set a content manager so new items can be spawned in.
             _contentManager = contentManager;
 
-            //Load all the assets that belong to level
+            // Load all the assets that belong to level
             _backgroundTexture = contentManager.Load<Texture2D>(_backgroundSourceName);
 
+            // Load all the content for each gameobject
             foreach (GameObject go in objects)
             {
                 if (!loadedSprites.ContainsKey(go.sprite.sourceName))
@@ -244,7 +250,7 @@ namespace SuperMarioWorld
                 go.sprite.texture = loadedSprites[go.sprite.sourceName];
             }
 
-            //Tell HUD to load its content too
+            // Tell HUD to load its content too
             _hud.LoadContent(contentManager);
         }
 
@@ -254,19 +260,20 @@ namespace SuperMarioWorld
         /// <param name="gameObject">GameObject that should be created</param>
         public void CreateObject(GameObject gameObject)
         {
-            //Create a temp object
+            // Create a temp object
             GameObject obj = gameObject;
 
-            //load in the sprites (if nesecarry)
+            // Load in the sprites (if nesecarry)
             if (loadedSprites.ContainsKey(obj.sprite.sourceName))
                 obj.sprite.texture = loadedSprites[obj.sprite.sourceName];
             else
                obj.sprite.texture = _contentManager.Load<Texture2D>(obj.sprite.sourceName);
 
-            //Add a create and destroy function
+            // Add a create and destroy function
             obj.create = new GameObject.CreateObject(CreateObject);
             obj.destroy = new GameObject.DestroyObject(DestroyObject);
 
+            // Add the object to the objects list
             objects.Add(obj);
         }
 
@@ -276,6 +283,7 @@ namespace SuperMarioWorld
         /// <param name="gameObject">Destroy this GameObject</param>
         public void DestroyObject(GameObject gameObject)
         {
+            // Remove the object
             objects.Remove(gameObject);
         }
 
@@ -285,42 +293,47 @@ namespace SuperMarioWorld
         /// <param name="gameTime"></param>
         public void Update(GameTime gameTime)
         {
+            // Get the current camera position 
             int camX = (int)cam.position.X;
             int camY = (int)cam.position.Y;
 
+            // Clear the array with objects that should have their collision checked
             _collidables.Clear();
 
-            //Give Builder object all the level information
+            // Give Builder object all the level information
             if (_player is Builder)
             {
-                Builder b = (Builder)_player;
-                b.allObjects = objects;
+                Builder builder = (Builder)_player;
+                builder.allObjects = objects;
             }
 
-            //TEMP 
+            // Kill the player when it falls out of the scene
             if(_player is Player)
             {
-                Player p = (Player)_player;
-                if (p.dead)
+                // Cast player
+                Player player = (Player)_player;
+                // Check if player is already dead
+                if (player.dead)
                 {
-                    if (p.position.Y >= _size.Y * _gridSize + 20)
+                    // Check if the player is outside of the map, then load the main menu
+                    if (player.position.Y >= _size.Y * _gridSize + 30)
                         SceneManager.Instance.LoadMainMenu();
                 }
-                else if(p.position.Y >= _size.Y * _gridSize)
+                else if(player.position.Y >= _size.Y * _gridSize + 20)
                 {
-                    p.powerState = Player.PowerState.small;
-                    p.Death(null);
+                    // Kill the player instantly
+                    player.powerState = Player.PowerState.small;
+                    player.Death(null);
                 }
             }
-            ///TEMP
 
-            //Call the update method for all gameobjects
+            // Call the update method for all gameobjects
             for (int i = 0; i < objects.Count; i++)
             {
-                //Check if the game object is within the screen
+                // Check if the game object is within the screen
                 if (Math.Abs(camX - objects[i].position.X) < cam.gameWidth)
                 {
-                    //Add the objects that are in the frame to the collision list to be checked for collisions
+                    // Add the objects that are in the frame to the collision list to be checked for collisions
                     if(objects[i] is StaticBlock)
                         _collidables.Add(objects[i]);
                     else if (Math.Abs(camX - objects[i].position.X) < cam.gameWidth / 1.5f)
@@ -333,10 +346,10 @@ namespace SuperMarioWorld
                         _collidables.Add(objects[i]);
                     }
 
-                    //Check if the object has fallen out of the map (downwards)
+                    // Check if the object has fallen out of the map (downwards)
                     if(objects[i].position.Y > _size.Y * _gridSize + 50)
                     {
-                        //Destroy the object if it is still alive.
+                        // Destroy the object if it is present in the objects array.
                         if(objects[i] != null)
                         {
                             objects[i].destroy(objects[i]);
@@ -345,15 +358,16 @@ namespace SuperMarioWorld
                 }
             }
 
-            //Check collisions
+            // Check collisions
             CheckCollisions();
 
-            //Update the camera position
+            // Update the camera position
             cam.Update(gameTime);
 
-            //Tell HUD to update
+            // Tell HUD to update
             _hud.Update(gameTime);
 
+            // Save level when <enter>  is pressed
             if (_edit && ( InputManager.Instance.KeyboardOnPress(Microsoft.Xna.Framework.Input.Keys.Enter) || InputManager.Instance.GamePadOnPress(Microsoft.Xna.Framework.Input.Buttons.Start)))
                 SaveLevel();
         }
@@ -363,15 +377,16 @@ namespace SuperMarioWorld
         /// </summary>
         public void CheckCollisions()
         {
-            //Cycle through every GameObject in _collidables
+            // Cycle through every GameObject in _collidables
             for(int i  = 0; i < _collidables.Count; i++)
             {
+                // Cycle through every gameobject in _collidables that comes after the first object (optimalisation)
                 for (int j = i + 1; j < _collidables.Count; j++)
                 {
-                    //Check if two objects collide
+                    // Check if two objects collide
                     if (_collidables[i].boundingBox.Intersects(_collidables[j].boundingBox))
                     {
-                        //Call the OnCollision functions of both objects.
+                        // Call the OnCollision functions of both objects.
                         _collidables[i].OnCollision(_collidables[j]);
                         _collidables[j].OnCollision(_collidables[i]);
                     }
@@ -384,10 +399,11 @@ namespace SuperMarioWorld
         /// </summary>
         public void DrawLevel(SpriteBatch batch)
         {
-            //Draw background(s)
+            // Draw background(s)
             int xPos = (int)Math.Round(cam.position.X / 2);
             int yPos = (int)Math.Round(cam.position.Y / 2);
 
+            // Draw the first background with parallax scrolling
             batch.Draw(_backgroundTexture, new Rectangle(_backOffset + xPos - _backgroundTexture.Width / 2, yPos - _backgroundTexture.Height / 2, _backgroundTexture.Width, _backgroundTexture.Height), Color.White);
 
             if (cam.position.X < _backOffset + xPos)
@@ -395,12 +411,13 @@ namespace SuperMarioWorld
             else
                 _backOffset += _backgroundTexture.Width;
 
+            // Draw the second background with parallax scrolling
             batch.Draw(_backgroundTexture, new Rectangle(_backOffset + xPos - _backgroundTexture.Width / 2, yPos - _backgroundTexture.Height / 2, _backgroundTexture.Width, _backgroundTexture.Height), Color.White);
 
-            //Draw every object
+            // Draw every object
             foreach (GameObject go in objects)
             {
-                //Call the draw method of gameobject
+                // Call the draw method of gameobject
                 go.DrawObject(batch);
             }
         }
@@ -411,7 +428,7 @@ namespace SuperMarioWorld
         /// <param name="batch"></param>
         public void DrawHUD(SpriteBatch batch)
         {
-            //Tell HUD to draw itself
+            // Tell HUD to draw itself
             _hud.DrawHUD(batch);
         }
 
@@ -420,20 +437,31 @@ namespace SuperMarioWorld
         /// </summary>
         public void SaveLevel()
         {
+            // Create a new XML Writer to write in a document
             XmlWriter writer = XmlWriter.Create(_name);
+
+            // Start with writing in a document
             writer.WriteStartDocument();
 
+            // Create a new element
             writer.WriteStartElement("Level");
+
+            // Give the new level an attribute xSize and ySize
             writer.WriteAttributeString("xSize", _size.X.ToString());
             writer.WriteAttributeString("ySize", _size.Y.ToString());
 
+            // Add all the objects in the level to the xml file
             foreach(GameObject obj in objects)
             {
+                // Call function that adds a gameobject to xml
                 AddGameObjectXML(writer, obj);
             }
 
+            // End
             writer.WriteEndElement();
+            // End
             writer.WriteEndDocument();
+            // End
             writer.Close();
         }
 
@@ -444,6 +472,7 @@ namespace SuperMarioWorld
         /// <param name="obj">the object to add</param>
         private void AddGameObjectXML(XmlWriter writer, GameObject obj)
         { 
+            // Checks which block is currently being writtten and writes it on the file
             if (obj is MysteryBlock)
             {
                 writer.WriteStartElement(obj.GetType().Name);
