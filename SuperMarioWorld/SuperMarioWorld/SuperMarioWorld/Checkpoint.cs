70aa5ae1e8755e6c3d59c95f1c62140bb394db5c
D Inbound <D_Inbound@hotmail.com>
2016-03-31 10:06:49 +0200

    Fixed comments for checkpoint.cs

--- SuperMarioWorld/SuperMarioWorld/SuperMarioWorld/Checkpoint.cs
+++ SuperMarioWorld/SuperMarioWorld/SuperMarioWorld/Checkpoint.cs
@@ -8,32 +8,41 @@ namespace SuperMarioWorld
 {
     public class Checkpoint : StaticObject
     {
+        // Variable to keep track if the object is a finish
         public bool isFinish;
 
         public Checkpoint(Point position, bool isFinish) : base(position)
         {
+            // A checkpoint does not block the player
             blocking = false;
+
+            // Set the finish to the finish
             this.isFinish = isFinish;
 
+            // Set the sprites size
             sprite.xSize = 32;
             sprite.ySize = 64;
 
+            // Set the sprites bounding box
             boundingBox = new Rectangle((int)position.X - 8, (int)position.Y - sprite.ySize, 16, 64);
 
+            // Set the animations for the sprite
             sprite.sourceName = "Blocks/checkpoint";
             sprite.AddFrame(0, 0);
             sprite.AddFrame(1, 0);
             sprite.AddFrame(2, 0);
             sprite.AddFrame(3, 0);
 
+            // Set the animation speed
             sprite.animationSpeed = 75.0f;
         }
 
         public override void OnCollision(GameObject collider)
         {
-            //check if this checkpoint is a finish
+            // Check if this checkpoint is a finish
             if (isFinish)
             {
+                // Do something when the player hits this object
                 if(collider is Player)
                 {
                     SceneManager.Instance.LoadMainMenu();
