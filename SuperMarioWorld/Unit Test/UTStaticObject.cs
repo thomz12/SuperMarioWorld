57290cc2f1051b3579ae4d59826b96cd34ed8042
D Inbound <D_Inbound@hotmail.com>
2016-04-01 09:10:48 +0200

    changes

--- SuperMarioWorld/Unit Test/UTStaticObject.cs
+++ SuperMarioWorld/Unit Test/UTStaticObject.cs
@@ -8,13 +8,17 @@ namespace SMWUT
     [TestClass]
     public class UTStaticObject
     {
+        TimeSpan totalGameTime;
+        TimeSpan elapsedGameTime;
+        GameTime gameTime;
+
         [TestInitialize]
         public void Initialize()
         {
-
-        }
-
-       
+            totalGameTime = new TimeSpan(0, 0, 0, 0, 2910);
+            elapsedGameTime = new TimeSpan(0, 0, 0, 0, 16);
+            gameTime = new GameTime(totalGameTime, elapsedGameTime);
+        }       
 
         [TestMethod]
         public void UTStaticBlockConstructor()
@@ -28,6 +32,16 @@ namespace SMWUT
         {
             StaticBlock sb = new StaticBlock(new Point(0, 0), StaticBlock.BlockType.rock, 0.5f);
             Assert.IsTrue(sb.blockType == StaticBlock.BlockType.rock);
+        }
+
+        [TestMethod]
+        public void UTStaticBlockOnCollision()
+        {
+            StaticBlock sb = new StaticBlock(new Point(0, 0), StaticBlock.BlockType.rock, 0.5f);
+            Player player = new Player(new Point(0, -16), null, Player.Character.Mario);
+            player.Update(gameTime);
+            sb.OnCollision(player);
+            Assert.IsFalse(player.boundingBox.Intersects(sb.boundingBox));
         }
     }
 }
